ce852835d5088c5e9d7a409af26007f3230330af
Dan Hedgecock <hedgecock.d@gmail.com>
2016-12-15 07:45:19 -0800

    Handle deduping and sorting classitems to prevent duplicate id errors

--- tests-lib/data-types/generate-classes-test.js
+++ tests-lib/data-types/generate-classes-test.js
@@ -17,6 +17,14 @@ const classes = {
     description: '<p>Some HTML</p>',
     file: 'another/file/path.js',
     extends: 'Ember.Component'
+  },
+  'lonely.Class': {
+    name: 'lonely.Class',
+    shortname: 'lonely.Class',
+    classitems: [],
+    description: '<p>Some HTML</p>',
+    file: 'another/lonely/path.js',
+    extends: 'Ember.Component'
   }
 };
 
@@ -25,8 +33,8 @@ const classItems = [
     file: 'some/file/path.js',
     line: 15,
     description: 'some `code`',
-    itemtype: 'property',
-    name: 'itemA',
+    itemtype: 'method',
+    name: 'itemC',
     type: '{Object}',
     class: 'someClass'
   },
@@ -34,8 +42,26 @@ const classItems = [
     file: 'some/file/path.js',
     line: 15,
     description: 'some `code`',
-    itemtype: 'method',
+    itemtype: 'property',
     name: 'itemB',
+    type: '{Object}',
+    class: 'someClass'
+  },
+  {
+    file: 'some/file/path.js',
+    line: 15,
+    description: 'This itemB should be used b/c its declared last',
+    itemtype: 'property',
+    name: 'itemB',
+    type: '{Object}',
+    class: 'someClass'
+  },
+  {
+    file: 'some/file/path.js',
+    line: 15,
+    description: 'Should end in the beginning',
+    itemtype: 'property',
+    name: 'itemA',
     type: '{Object}',
     class: 'someClass'
   },
@@ -52,7 +78,7 @@ const classItems = [
 
 describe('generate-classes', function() {
   it('returns empty arrays if there is no data', () => {
-    const actual = generateClasses({}, {});
+    const actual = generateClasses({}, []);
 
     assert.ok(Array.isArray(actual.classesMeta), 'returns empty array even if there is no data');
     assert.ok(Array.isArray(actual.classesDatas), 'returns empty array even if there is no data');
@@ -62,8 +88,9 @@ describe('generate-classes', function() {
     const testClasses = JSON.parse(JSON.stringify(classes));
     const testClassItems = JSON.parse(JSON.stringify(classItems));
     const expected = [
-      { name: 'someClass', type: 'classes' },
-      { name: 'anotherClass', type: 'classes' }
+      { name: 'anotherClass', type: 'classes' },
+      { name: 'lonely.Class', type: 'classes' },
+      { name: 'someClass', type: 'classes' }
     ];
     const actual = generateClasses(testClasses, testClassItems).classesMeta;
 
@@ -76,23 +103,31 @@ describe('generate-classes', function() {
     const actual = generateClasses(testClasses, testClassItems);
     const someClass = actual.classesDatas.filter(klass => klass.name === 'someClass')[0];
     const anotherClass = actual.classesDatas.filter(klass => klass.name === 'anotherClass')[0];
+    const lonelyClass = actual.classesDatas.filter(klass => klass.name === 'lonely.Class')[0];
 
     assert.ok(Array.isArray(actual.classesMeta), 'returns array of class meta');
     assert.ok(Array.isArray(actual.classesDatas), 'returns array of class objects');
 
     assert.ok(someClass, 'returns class someClass in results');
     assert.ok(anotherClass, 'returns class anotherClass in results');
+    assert.ok(lonelyClass, 'returns class lonely.Class in results');
 
-    assert.equal(someClass.classitems.length, 2, 'all classitems for someClass are attached');
+    assert.equal(someClass.classitems.length, 3, 'someClass should have only 3 classes after deduping');
     assert.equal(anotherClass.classitems.length, 1, 'all classitems for anotherClass are attached');
+    assert.equal(lonelyClass.classitems.length, 0, 'lonelyClass has no class items');
 
     assert.ok(Array.isArray(someClass.method), 'method array is added to classes');
     assert.ok(Array.isArray(someClass.property), 'property array is added to classes');
     assert.ok(Array.isArray(someClass.event), 'event array is added to classes');
 
     assert.equal(someClass.method.length, 1, 'method class item was attached to correct class');
-    assert.equal(someClass.property.length, 1, 'property class item was attached to correct class');
+    assert.equal(someClass.property.length, 2, 'property class item was attached to correct class');
     assert.equal(someClass.event.length, 0, 'no event class items returns empty array for class');
+
+    assert.equal(someClass.property[0].name, 'itemA', 'class items are sorted alphabetically');
+    assert.equal(someClass.property[1].description,
+      '<p>This itemB should be used b/c its declared last</p>\n',
+      'duplicate itemB should pick up last declared item');
 
     assert.equal(anotherClass.method.length, 0, 'no method class items returns empty array for class');
     assert.equal(anotherClass.property.length, 0, 'no property class items returns empty array for class');
