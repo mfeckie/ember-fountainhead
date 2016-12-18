8b88f9cad96a2ee1d4aba63cbac71d9a317dc594
Dan Hedgecock <hedgecock.d@gmail.com>
2016-12-17 16:35:19 -0800

    Broccoli filter not excluding fountainhead components

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 'use strict';
-const path = require('path');
+// const path = require('path');
+// const Funnel = require('broccoli-funnel');
 
-let config;
+// let config;
+//
+// // Configuration is optional, load in a try/catch to handle possible error
+// try {
+//   config = require(path.resolve('fountainhead.js'));
+// } catch(ex) {
+//   config = {};
+// }
 
-// Configuration is optional, load in a try/catch to handle possible error
-try {
-  config = require(path.resolve('fountainhead.js'));
-} catch(ex) {
-  config = {};
-}
-
-console.log('FOUNTAINHEAD: ', config);
+// console.log('FOUNTAINHEAD: ', config);
 
 module.exports = {
   name: 'ember-fountainhead',
@@ -19,21 +20,18 @@ module.exports = {
   // ---------------------------------------------------------------------------
 
   _importBrowserDependencies(app) {
-    // const vendor = this.treePaths.vendor;
-
     // Required to compile templates at runtime
     app.import('bower_components/ember/ember-template-compiler.js');
-
-    // Ember Radical Styles
-    // @TODO: Make this configurable
-    // Not ready for these styles
-    // app.import(path.join(vendor, 'ember-radical', 'styles.css'));
   },
 
   // Addon Hooks
   // ---------------------------------------------------------------------------
 
-  included: function(app, parentAddon) {
+  /**
+   * Handle importing browser dependencies
+   * @method included
+   */
+  included(app, parentAddon) {
     this._super.included.apply(this, arguments);
 
     // see: https://github.com/ember-cli/ember-cli/issues/3718
@@ -46,21 +44,20 @@ module.exports = {
     // Import Dependencies
     this._importBrowserDependencies(app);
   }
-
-  // treeForVendor: function(vendorTree) {
-  //   const trees = [];
-  //   let radicalPath = path.dirname(require.resolve('ember-radical')); // MAGIC!
-  //
-  //   // Pull in existing vendor tree
-  //   if (vendorTree) {
-  //     trees.push(vendorTree);
-  //   }
-  //
-  //   // Pull in ember-radical style files
-  //   trees.push(new Funnel(path.resolve(radicalPath, 'vendor'), {
-  //     include: [new RegExp(/\.css$/)]
-  //   }));
-  //
-  //   return mergeTrees(trees);
+  /**
+   * Handle excluding files in prod build unless configured to include.
+   * @method postprocessTree
+   */
+  // postprocessTree(type, tree) {
+  //   // We only want to work with the js files
+  //   if (type !== 'js') { return tree; }
+  //   // If configured for production build, do less
+  //   if (config.includeForProduction) { return tree; }
+  //   // Use Funnel to exclude components from build
+  //   console.log('new funnel time');
+  //   return new Funnel(tree, {
+  //     exclude: ['components/fountain-head/**/*'],
+  //     description: 'Funnel: Remove Fountainhead components for production build'
+  //   });
   // }
 };
