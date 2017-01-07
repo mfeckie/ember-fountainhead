56384d0ea7673e62a5c1f3325cf43c9412174fba
Dan Hedgecock <hedgecock.d@gmail.com>
2017-01-07 11:41:35 -0800

    More work on the demo app home page

--- tests/dummy/app/components/sidebar-nav.js
+++ tests/dummy/app/components/sidebar-nav.js
@@ -5,7 +5,7 @@ export default Sidebar.extend({
   layout: hbs`
     {{#fountainhead-sidebar/section title='Ember Fountainhead'}}
       <ul class="item-group">
-        <li class="item-group-item">{{link-to 'Home' 'application' classNames='item-link'}}</li>
+        <li class="item-group-item">{{link-to 'Home' 'index' classNames='item-link'}}</li>
         <li class="item-group-item">{{link-to 'Getting Started' 'getting-started' classNames='item-link'}}</li>
         <li class="item-group-item">{{link-to 'Demo' 'docs' classNames='item-link'}}</li>
       </ul>
