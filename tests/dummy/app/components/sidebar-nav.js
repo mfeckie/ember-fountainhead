fbdd3ffcd8faefb366e93138638288cb7076ea8c
Dan Hedgecock <hedgecock.d@gmail.com>
2017-01-07 19:25:24 -0800

    Add getting started instructions for demo app

--- tests/dummy/app/components/sidebar-nav.js
+++ tests/dummy/app/components/sidebar-nav.js
@@ -1,14 +1,19 @@
+import Component from 'ember-component';
 import hbs from 'htmlbars-inline-precompile';
-import Sidebar from 'ember-fountainhead/components/fountainhead-sidebar/sidebar'
 
-export default Sidebar.extend({
+/**
+ * Demo app sidebar will likely be the sidebar that we use for guides.
+ * @class SidebarNav
+ * @constructor
+ * @extends Ember.Component
+ */
+export default Component.extend({
   layout: hbs`
-    {{#fountainhead-sidebar/section title='Ember Fountainhead'}}
-      <ul class="item-group">
-        <li class="item-group-item">{{link-to 'Home' 'index' classNames='item-link'}}</li>
-        <li class="item-group-item">{{link-to 'Getting Started' 'getting-started' classNames='item-link'}}</li>
-        <li class="item-group-item">{{link-to 'Demo' 'docs' classNames='item-link'}}</li>
+    <nav>
+      <ul>
+        <li>{{link-to 'Home' 'index' classNames='item-link'}}</li>
+        <li>{{link-to 'Demo' 'docs' classNames='item-link'}}</li>
       </ul>
-    {{/fountainhead-sidebar/section}}
+    </nav>
   `
 });
