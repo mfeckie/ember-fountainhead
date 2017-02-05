ddef1449769d078f0549fcd82b152a168aba696b
Dan Hedgecock <hedgecock.d@gmail.com>
2017-02-05 13:46:28 -0800

    Huzzah, no more Bower :tada: Update to Ember 2.11

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,11 +2,6 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    // Disable JSHint by telling ember-cli-qunit not to use the lintTree
-    'ember-cli-qunit': {
-      useLintTree: false
-    },
-
     // Don't fingerprint the ember-logo b/c it is pulled using a path from the
     // docs meta which doesn't get updated with fingerprint hash
     fingerprint: {
